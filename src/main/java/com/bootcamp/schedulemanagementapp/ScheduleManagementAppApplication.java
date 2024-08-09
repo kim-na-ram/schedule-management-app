0c0d48eb04cca58e576767d7caea8567ff4c08ae
kim-na-ram <snrneh3@naver.com>
2024-08-09 13:37:23 +0900

    [ADD] 프로젝트 생성 및 초기 세팅

---
+++ src/main/java/com/bootcamp/schedulemanagementapp/ScheduleManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.schedulemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/schedulemanagementapp/ScheduleManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.schedulemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
